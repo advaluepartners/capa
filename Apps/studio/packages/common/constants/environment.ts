8fea3981ae2e4390c720a445db41b81ff46e9e2b
advaluepartners <bjc@advalue.partners>
2024-07-06 00:40:39 +0100

    3rd commit

--- Apps/studio/packages/common/constants/environment.ts
+++
@@ -1,3 +0,0 @@
-export const IS_PLATFORM = process.env.NEXT_PUBLIC_IS_PLATFORM === 'false'
-//export const IS_PROD = process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
-export const IS_PROD = process.env.NEXT_PUBLIC_NODE_ENV === 'test'
