9f2ae8d5e0c39a19e0002c62f588aaee5d132047
advaluepartners <bjc@advalue.partners>
2024-07-16 15:55:17 +0100

    34th commit

--- Apps/studio/.eslintrc.js
+++ Apps/studio/.eslintrc.js
@@ -6,18 +6,22 @@ module.exports = {
     'prettier'
   ],
   plugins: ['@typescript-eslint'],
+  // rules: {
+  //   '@next/next/no-img-element': 'off',
+  //   'react/no-unescaped-entities': 'off',
+  //   'react/display-name': 'warn',
+  // }
   rules: {
     '@next/next/no-img-element': 'off',
     'react/no-unescaped-entities': 'off',
-    'react/display-name': 'warn',
+    'react/display-name': 'off',
+    'no-console': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
   }
+
 };
 
-// module.exports = {
-//   extends: ['eslint-config-supabase/next'],
-//   rules: {
-//     '@next/next/no-img-element': 'off',
-//     'react/no-unescaped-entities': 'off',
-//     'react/display-name': 'warn',
-//   },
-// }
